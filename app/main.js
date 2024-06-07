c47f138aacef659664ce92b77e1f0045fbd24a03
oliwer <olwrrb@protonmail.com>
2024-06-07 15:10:11 -0300

    add favicon and partners link logic, change iframe size on big screens

--- app/main.js
+++ app/main.js
@@ -1,5 +1,16 @@
 import showPage from "./pages.js";
 import start from "./show.js";
 
+const subBtn = document.querySelector('#sub-form-button');
+const partners = window.location.search;
+
+subBtn.action = `https://plataforma.proa.org.br/login/signup.php${partners}`;
+
+// subBtn.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     window.location.href = `https://plataforma.proa.org.br/login/signup.php${partners}`
+// })
+
 showPage();
 start();
